280c3be44ad5161f2f9c4b14aefadc808114a86f
kingofdead6 <ny_kahlouche@esi.dz>
2025-07-08 15:45:54 +0100

    Added new features

--- client/src/components/Home/Works.jsx
+++ client/src/components/Home/Works.jsx
@@ -102,7 +102,7 @@ const ProjectCard = ({
         </div>
         <div className="mt-5">
           <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-[#aaa6c3] text-[14px] line-clamp-3">{description}</p>
+          <p className="mt-2 text-[#aaa6c3] text-[14px] ">{description}</p>
         </div>
         <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
